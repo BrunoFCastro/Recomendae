a047409a6514b8cbbfca8305e8458ec77cc0eb16
BrunoFCastro <brunoeugat@gmail.com>
2023-07-02 22:44:15 -0300

    adição de função ao BookItem

--- frontend/src/Componentes/MenuLateral/MenuLateral.jsx
+++ frontend/src/Componentes/MenuLateral/MenuLateral.jsx
@@ -16,3 +16,39 @@ function MenuLateral(){
 }
 
 export default MenuLateral
+
+// // Menu lateral
+
+// // Seleciona o ícone do menu
+// const menuIcon = document.querySelector('.menu-icon');
+// // Seleciona o menu
+// const menu = document.querySelector('.menu');
+
+// // Adiciona um ouvinte de evento de clique ao ícone do menu
+// menuIcon.addEventListener('click', toggleMenu);
+
+// // Alterna a classe 'active' no menu ao clicar no ícone do menu
+// function toggleMenu() {
+//   menu.classList.toggle('active');
+// }
+
+// // Seleciona o ícone de fechar
+// const closeIcon = document.querySelector('.close-icon');
+
+// // Adiciona um ouvinte de evento de clique ao ícone de fechar
+// closeIcon.addEventListener('click', closeMenu);
+
+// // Função para fechar o menu
+// function closeMenu() {
+//   menu.classList.remove('active');
+// }
+
+// // Adiciona um ouvinte de evento de clique no documento inteiro
+// document.addEventListener('click', function(event) {
+//   var targetElement = event.target;
+
+//   // Verifica se o clique foi fora da barra lateral ou do ícone do menu
+//   if (!menu.contains(targetElement) && !menuIcon.contains(targetElement)) {
+//     menu.classList.remove('active');
+//   }
+// });
